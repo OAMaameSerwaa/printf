34aacebb8b8cf3b60243bbafbb79d35de1448fe5
OAMaameSerwaa <owusuansahharriet1991@gmail.com>
2022-11-21 09:06:55 -0800

    Task 0

---
+++ _printf.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * _printf - Printf function
+ * @format: format
+ * Return: chars printed
+ */
+int _printf(const char *format, ...)
+{
+	int a = 0, b = 0, buff_ct = 0, prev_buff_ct = 0;
+	char buffer[2000];
+	va_list list;
+	call_t container[] = {
+		{'c', parse_char}, {'s', parse_str}, {'i', parse_int}, {'d', parse_int},
+		{'%', parse_perc}, {'b', parse_bin}, {'o', parse_oct}, {'x', parse_hex},
+		{'X', parse_X}, {'u', parse_uint}, {'R', parse_R13}, {'r', parse_rev},
+		{'\0', NULL}
+	};
+
+	if (!format)
+		return (-1);
+	va_start(list, format);
+	while (format && format[a] != '\0')
+	{
+		if (format[a] == '%')
+		{
+			a++, prev_buff_ct = buff_ct;
+			for (b = 0; container[b].t != '\0'; b++)
+			{
+				if (format[a] == '\0')
+					break;
+				if (format[a] == container[b].t)
+				{
+					buff_ct = container[b].fn(buffer, list, buff_ct);
+					break;
+				}
+			}
+			if (buff_ct == prev_buff_ct && format[a])
+				a--, buffer[buff_ct] = format[a], buff_ct++;
+		}
+		else
+			buffer[buff_ct] = format[a], buff_ct++;
+		a++;
+	}
+	va_end(list);
+	buffer[buff_ct] = '\0';
+	print_buff(buffer, buff_ct);
+	return (buff_ct);
+}
+
--- main.h
+++ main.h
@@ -9,4 +9,31 @@
 #include <limits.h>
 
 /**
- *
+ * struct call - struct call
+ * @t: t - flag for data type
+ * @fn: function - assocated function
+ */
+typedef struct call
+{
+	char t;
+	int (*fn)(char *, va_list, int);
+} call_t;
+
+int _printf(const char *format, ...);
+int parse_R13(char *buff_dest, va_list arg, int buff_count);
+int buff_append(char *buff_dest, va_list arg, int buff_count, char type);
+int parse_rev(char *buff_dest, va_list arg, int buff_count);
+int print_buff(char *buff, unsigned int nbuff);
+int parse_uint(char *buff_dest, va_list arg, int buff_count);
+int str_len(char *s);
+int parse_X(char *buff_dest, va_list arg, int buff_count);
+char *_strcpy(char *dest, char *src);
+int parse_hex(char *buff_dest, va_list arg, int buff_count);
+int parse_char(char *buff_dest, va_list arg, int buff_count);
+int parse_oct(char *buff_dest, va_list arg, int buff_count);
+int parse_str(char *buff_dest, va_list arg, int buff_count);
+int parse_bin(char *buff_dest, va_list arg, int buff_count);
+int parse_perc(char *buff_dest, va_list arg, int buff_count);
+int parse_int(char *buff_dest, va_list list, int buff_count);
+
+#endif
